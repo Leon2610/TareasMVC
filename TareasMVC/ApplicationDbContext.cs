5223e8c095dbeb559af24fbd1b678d31f43a094f
Leonel Leon <leonleo1996@gmail.com>
2023-06-03 22:40:26 -0600

    Creación de 1er entidad

--- TareasMVC/ApplicationDbContext.cs
+++ TareasMVC/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TareasMVC.Entidades;
 
 namespace TareasMVC
 {
@@ -7,5 +8,7 @@ namespace TareasMVC
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
+
+        public DbSet<Tarea> Tareas { get; set; }
     }
 }
