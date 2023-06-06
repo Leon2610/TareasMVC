410123a09f0ad7543bcc57a25b151f539c7506be
Leonel Leon <leonleo1996@gmail.com>
2023-06-05 22:34:45 -0600

    Registro de Usuarios

--- TareasMVC/Program.cs
+++ TareasMVC/Program.cs
@@ -1,12 +1,19 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using TareasMVC;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var politicaUsuariosAutenticados = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(opciones =>
+{
+    opciones.Filters.Add(new AuthorizeFilter(politicaUsuariosAutenticados));
+});
 
 builder.Services.AddDbContext<ApplicationDbContext>(opciones => opciones.UseSqlServer("name=DefaultConnection"));
 builder.Services.AddAuthentication();
@@ -14,7 +21,7 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>(opciones => opciones.Si
     .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.PostConfigure<CookieAuthenticationOptions>(IdentityConstants.ApplicationScheme, opciones =>
                                                                                             {
-                                                                                                opciones.LoginPath = "usuarios/login";
+                                                                                                opciones.LoginPath = "/usuarios/login";
                                                                                                 opciones.AccessDeniedPath = "/usuarios/login";
                                                                                             }) ;
 
