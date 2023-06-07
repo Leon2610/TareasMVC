8b35079e62f31c36afed110e5f8c7cff394e1907
Leonel Leon <leonleo1996@gmail.com>
2023-06-06 20:27:34 -0600

    Roles

---
+++ TareasMVC/Migrations/20230607022047_AdminRol.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TareasMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminRol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"IF NOT EXISTS(SELECT Id FROM AspNetRoles WHERE Id = 'dc2c0431-73e9-4573-af1a-f5dfac034166')
+                                BEGIN
+	                                INSERT AspNetRoles (Id, [Name], [NormalizedName])
+	                                VALUES ('dc2c0431-73e9-4573-af1a-f5dfac034166','admin', 'ADMIN')
+                                END");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE AspNetRoles WHERE Id = 'dc2c0431-73e9-4573-af1a-f5dfac034166'");
+        }
+    }
+}
