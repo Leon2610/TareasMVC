171b0221d2078bfafb8be087cbc116c6ee50765c
Leonel Leon <leonleo1996@gmail.com>
2023-06-08 21:52:50 -0600

    Agregando una nueva tarea a la base de datos

--- TareasMVC/Controllers/TareasController.cs
+++ TareasMVC/Controllers/TareasController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TareasMVC.Entidades;
 using TareasMVC.Servicios;
 
 namespace TareasMVC.Controllers
@@ -13,6 +15,34 @@ namespace TareasMVC.Controllers
         {
             this.context = context;
             this.servicioUsuarios = servicioUsuarios;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Tarea>> Post([FromBody] string titulo)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+
+            var existenTareas = await context.Tareas.AnyAsync(t => t.UsuarioCreacionId == usuarioId);
+
+            var ordenMayor = 0;
+
+            if (existenTareas)
+            {
+                ordenMayor = await context.Tareas.Where(t => t.UsuarioCreacionId == usuarioId).Select(t => t.Orden).MaxAsync();
+            }
+
+            var tarea = new Tarea()
+            {
+                Titulo = titulo,
+                UsuarioCreacionId = usuarioId,
+                FechaCreacion = DateTime.UtcNow,
+                Orden = ordenMayor + 1
+            };
+
+            context.Add(tarea);
+            await context.SaveChangesAsync();
+
+            return tarea;
         }
     }
 }
