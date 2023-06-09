f7a38afca57d00074da0a6f06765803aa23f7a7a
Leonel Leon <leonleo1996@gmail.com>
2023-06-08 22:37:27 -0600

    Mostrando Errores con SweetAlert2

--- TareasMVC/wwwroot/js/tareas.js
+++ TareasMVC/wwwroot/js/tareas.js
@@ -25,7 +25,7 @@ async function manejarFocusoutTituloTarea(tarea) {
         const json = await respuesta.json();
         tarea.id(json.id);
     } else {
-        //Mostrar mensaje de error
+        manejarErrorApi(respuesta);
     }
 }
 
@@ -40,6 +40,7 @@ async function obtenerTareas() {
     });
 
     if (!respuesta.ok) {
+        manejarErrorApi(respuesta);
         return;
     }
 
